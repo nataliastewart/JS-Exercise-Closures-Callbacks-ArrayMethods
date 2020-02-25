da95160147adb35d39414185fa21701233ea6168
Natalia-Stewart <nataliamirandac@hotmail.com>
2020-02-25 17:18:49 -0500

    Finished processProduct

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ function processSum(numberList, callback) {
  * should return 1000.
  */
 function processProduct(num1, num2, callback) {
-  return callback((num1, num2) => {
-    return num1 * num2);
+  return callback(num1 * num2);
+}
 
 /**
  * ### Challenge `processContains`
