bdbb79c47a894f6a3cd75721e1c8be3c4519a981
Natalia-Stewart <nataliamirandac@hotmail.com>
2020-02-25 22:17:44 -0500

    First name uppercase DONE

--- index.js
+++ index.js
@@ -201,8 +201,11 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
  */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let newArray = runners.map(function callback(item) {
+    return item.first_name.toUpperCase();
+  });
+  return newArray;
 }
 
 /**
